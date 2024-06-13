d3f10ebd89d274ee1a7c84c13cb148484096dd1e
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 15:42:45 +0900

    [refactoring] extract methods (43)

--- baseball.py
+++ baseball.py
@@ -1,14 +1,19 @@
 class BaseballGame:
     def guess(self, guess_number):
+        self.check_input_validity(guess_number)
+
+    def check_input_validity(self, guess_number):
         if guess_number is None:
             raise TypeError
-
         if len(guess_number) != 3:
             raise TypeError
-
         for c in guess_number:
             if not c.isdigit():
                 raise TypeError
-
-        if guess_number[0] == guess_number[1] or guess_number[1] == guess_number[2] or guess_number[0] == guess_number[2]:
+        if self.check_duplication(guess_number):
             raise TypeError
+
+    def check_duplication(self, guess_number):
+        return guess_number[0] == guess_number[1] or \
+            guess_number[1] == guess_number[2] or \
+            guess_number[0] == guess_number[2]
