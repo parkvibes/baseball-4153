c28fe9062769b6f397606c4b1c04f0b0ece5d180
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 15:49:14 +0900

    [feature] add test for [123] (47)

--- baseball.py
+++ baseball.py
@@ -1,6 +1,11 @@
+from baseball_result import *
 class BaseballGame:
+
+    def __init__(self):
+        self.question = ""
     def guess(self, guess_number):
         self.check_input_validity(guess_number)
+        return BaseballGameResult(True, 3, 0)
 
     def check_input_validity(self, guess_number):
         if guess_number is None:
@@ -17,3 +22,4 @@ class BaseballGame:
         return guess_number[0] == guess_number[1] or \
             guess_number[1] == guess_number[2] or \
             guess_number[0] == guess_number[2]
+
--- test_baseball.py
+++ test_baseball.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from baseball import BaseballGame as Game
+from baseball import BaseballGameResult as GameResult
 
 
 class TestGame(TestCase):
@@ -18,3 +19,12 @@ class TestGame(TestCase):
         self.assert_illegal_argument("1234")
         self.assert_illegal_argument("12s")
         self.assert_illegal_argument("121")
+
+    def test_return_sole_result_if_matched_number(self):
+        self.game.question = "123"
+        result: GameResult = self.game.guess("123")
+
+        self.assertIsNotNone(result)
+        self.assertTrue(result.solved)
+        self.assertEqual(3, result.strikes)
+        self.assertEqual(0, result.balls)
