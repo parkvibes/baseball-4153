b4bc12ee8f92b44f557aa3d0cbfec834cf2d86a0
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 15:34:30 +0900

    [feature] check non-digit input

--- baseball.py
+++ baseball.py
@@ -5,3 +5,7 @@ class BaseballGame:
 
         if len(guess_number) != 3:
             raise TypeError
+
+        for c in guess_number:
+            if c < '0' and c > '9':
+                raise TypeError
