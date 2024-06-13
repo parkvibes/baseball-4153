70b8b5a7cc0a6050040b256afbe64daca90db4fd
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 15:25:32 +0900

    [feature] check validity(30)

--- baseball.py
+++ baseball.py
@@ -2,3 +2,6 @@ class BaseballGame:
     def guess(self, guess_number):
         if guess_number is None:
             raise TypeError
+
+        if len(guess_number) != 3:
+            raise TypeError
